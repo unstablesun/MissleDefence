b9085253e1a1faf7382ebbe8f50d0becaa05cb74
Dave Hards <davehards@Daves-Mac-mini.local>
2017-06-30 13:12:56 -0700

    custom editor testbed

--- Assets/Editor/CustomScriptEditor.cs
+++ Assets/Editor/CustomScriptEditor.cs
@@ -11,14 +11,16 @@ public class ProgramListEditor : Editor
 
 	}
 
+	public GUIStyle mystyle;
 
 	public override void OnInspectorGUI()
 	{
 		base.OnInspectorGUI ();
 
+		mystyle = new GUIStyle ();
 
-
-		GUILayout.Label ("My Custom Control");
+		mystyle.normal.textColor = Color.green; 
+		GUILayout.Label ("My Custom Control", mystyle);
 
 		GUILayout.Space (20);
 
@@ -29,9 +31,16 @@ public class ProgramListEditor : Editor
 
 		CustomInspectorTest myTarget = (CustomInspectorTest)target;
 
+		//GUI.contentColor = Color.white;
+
 		GUI.color = Color.cyan;
 
 		myTarget.NumEntriesUsed = EditorGUILayout.IntField("Experience", myTarget.NumEntriesUsed);
+
+		GUI.color = new Color(0.25f, 0.35f, 0.45f);
+
+		//GUI.
+		EditorGUILayout.Vector3Field ("V3", new Vector3(0,0,0));
 
 		//EditorGUILayout.LabelField("Level", myTarget.Level.ToString());
 
