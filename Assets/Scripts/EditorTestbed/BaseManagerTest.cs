d9adc730edf5c13d802544dc16015416d4a3e493
Dave Hards <davehards@Daves-Mac-mini.local>
2017-07-24 16:33:48 -0700

    doTween cloud animation test

---
+++ Assets/Scripts/EditorTestbed/BaseManagerTest.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using EZObjectPools;
+using DG.Tweening;
+
+public class BaseManagerTest : MonoBehaviour 
+{
+	public EZObjectPool objectPool;
+
+
+	void Awake ()
+	{
+		DOTween.Init(false, true, LogBehaviour.ErrorsOnly);
+
+		DOTween.defaultEaseType = Ease.Linear;
+		DOTween.defaultEasePeriod = 0f;
+	}
+
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		GameObject go = null;
+
+		float rx = (float)Random.Range (-14f, -24f);
+		float ry = (float)Random.Range (6f, 10f);
+		float rz = (int)Random.Range (0f, -4f);
+
+		objectPool.TryGetNextObject(new Vector3(rx, ry, rz), new Quaternion(), out go);
+
+	
+		if (go != null) {
+
+			BaseSpriteObj objectScript = go.GetComponent<BaseSpriteObj> ();
+			objectScript.StartAnimTest (rx, ry, rz);
+
+			float grey = (float)Random.Range (32f, 160f);
+			float alpha = (float)Random.Range (32f, 200f);
+			go.GetComponent<Renderer> ().material.color = new Color32 ((byte)grey, (byte)grey, (byte)grey, (byte)alpha);
+
+
+
+
+		}
+
+		
+	}
+}
---
+++ Assets/Scripts/Util/BaseSpriteObj.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using EZObjectPools;
+using DG.Tweening;
+
+using DG.Tweening.Core;
+using DG.Tweening.Plugins.Core.PathCore;
+using DG.Tweening.Plugins.Options;
+
+
+public class BaseSpriteObj : PooledObject 
+{
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+
+	public void Init () 
+	{
+
+	}
+
+	public void StartAnimTest (float dx, float dy, float dz) 
+	{
+		float offsetx = (float)Random.Range (0f, 8f);
+		float speed = (float)Random.Range (12f, 20f);
+
+		transform.DOMove(new Vector3(24f + offsetx, 0, 0), speed).SetRelative().SetLoops(-1, LoopType.Restart);
+	}
+
+
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+}
