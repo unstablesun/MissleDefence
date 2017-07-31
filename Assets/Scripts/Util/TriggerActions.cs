e300562b006668e03ae1ed95b7f3af5606b91034
Dave Hards <davehards@Daves-Mac-mini.local>
2017-07-31 16:26:02 -0700

    node canvas tests

---
+++ Assets/Scripts/Util/TriggerActions.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NodeCanvas;
+
+public class TriggerActions : MonoBehaviour 
+{
+
+	void Start () 
+	{
+		GetComponent<ActionListPlayer> ().Play ();
+	}
+	
+}
